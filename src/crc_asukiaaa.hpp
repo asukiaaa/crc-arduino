cd20327ff9a30488d335d2e40d917a07a77eeaa9
Asuki Kono <asukiaaa@gmail.com>
2024-02-06 09:08:31 +0900

    Assign const to checkCrc16AtEnd

--- src/crc_asukiaaa.hpp
+++ src/crc_asukiaaa.hpp
@@ -14,7 +14,7 @@ void setCrc16AtEnd(uint8_t* dataArr, uint8_t dataLenIncludesCrc) {
   dataArr[dataLenIncludesCrc - 1] = crc16 & 0xff;
 }
 
-bool checkCrc16AtEnd(uint8_t* dataArr, size_t dataLenIncludesCrc) {
+bool checkCrc16AtEnd(const uint8_t* dataArr, size_t dataLenIncludesCrc) {
   uint16_t crc16Created = crcx::crc16(dataArr, dataLenIncludesCrc - 2);
   uint16_t crc16Received = ((uint16_t)dataArr[dataLenIncludesCrc - 2] << 8) +
                            dataArr[dataLenIncludesCrc - 1];
